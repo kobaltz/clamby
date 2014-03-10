f98613e83e7bb9ca8fbabf47369aeadf08c953fd
kobaltz <dave@k-innovations.net>
2014-03-10 18:55:23 -0400

    Fixed spacing

--- lib/clamby.rb
+++ lib/clamby.rb
@@ -11,44 +11,44 @@ module Clamby
   end
 
   def self.scan(path)
-  	if self.scanner_exists?
-  		if file_exists?(path)
-  			scanner = system("clamscan #{path} --no-summary")
-			if scanner
-				return true
-			elsif not scanner
-				puts "VIRUS DETECTED on #{Time.now}: #{path}"
-				return false
-			end
-		end
-	end
+    if self.scanner_exists?
+      if file_exists?(path)
+        scanner = system("clamscan #{path} --no-summary")
+        if scanner
+          return true
+        elsif not scanner
+          puts "VIRUS DETECTED on #{Time.now}: #{path}"
+          return false
+        end
+      end
+    end
   end
 
   def self.scanner_exists?
-  	if @config[:check]
-	  	scanner = system('clamscan')
-	  	if not scanner
-	  		puts "CLAMSCAN NOT FOUND"
-	  		return false
-	  	else
-	  		return true
-	  	end
-	else
-		return true
-	end
+    if @config[:check]
+      scanner = system('clamscan')
+      if not scanner
+        puts "CLAMSCAN NOT FOUND"
+        return false
+      else
+        return true
+      end
+  else
+    return true
+  end
   end
 
   def self.file_exists?(path)
-  	if File.file?(path)
-  		return true
-	else
-		puts "FILE NOT FOUND on #{Time.now}: #{path}"
-		return false
-	end
+    if File.file?(path)
+      return true
+  else
+    puts "FILE NOT FOUND on #{Time.now}: #{path}"
+    return false
+  end
   end
 
   def self.update
-  	system("freshclam")
+    system("freshclam")
   end
 
   def self.config
