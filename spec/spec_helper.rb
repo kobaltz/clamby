632712569fa85926c6f9fabe6819d2011675e330
Michał Szajbe <michal.szajbe@gmail.com>
2018-11-15 13:03:13 +0100

    Fix test for dangerous file downloaded from eicar site

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,30 @@
 require 'bundler/setup'
 Bundler.setup
 
+require 'open-uri'
+require 'tempfile'
+
 require 'clamby' # and any other gems you need
 
 RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     # so that Command can keep doing what it always does.
     mocks.verify_partial_doubles = true
+  end
+
+  def download(url)
+    file = open(url)
+    file.is_a?(StringIO) ? to_tempfile(file) : file
+  end
+
+  # OpenURI returns either Tempfile or StringIO depending of the size of
+  # the response. We want to unify this and always return Tempfile.
+  def to_tempfile(io)
+    tempfile = Tempfile.new('tmp')
+    tempfile.binmode
+    ::OpenURI::Meta.init(tempfile, io)
+    tempfile << io.string
+    tempfile.rewind
+    tempfile
   end
 end
